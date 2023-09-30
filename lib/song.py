44ec2c4aab76d1c6db91141898119fd2072efb38
Daniel Young <Danyoung91@gmail.com>
2023-09-30 00:16:14 -0400

    done

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,18 @@
-class Song:
-    pass
+class Song():
+    
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+    
+    def __init__(self, name, artist, genre) -> None:       
+        self.name, self.artist, self.genre = name, artist, genre
+        Song.count += 1
+        Song.genres.append(genre)
+        Song.artists.append(artist)        
+        Song.genre_count[genre] = Song.genre_count[genre] + 1 if genre in Song.genre_count else 1
+        Song.artist_count[artist] = Song.artist_count[artist] + 1 if artist in Song.artist_count else 1
+        
+
+        
